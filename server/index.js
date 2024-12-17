ff8ae6b37cd8da75dcd89c7a08a5ceda3ae6c213
RobertMarc19 <mogyorosi.robert08@gmail.com>
2024-12-17 19:16:16 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,10 +2,10 @@ const express = require("express");
 const connection = require("../database/connection");
 const path = require("path");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const PORT = 3000;
 
-app.use(cors()); 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "..")));
@@ -19,22 +19,30 @@ app.post("/save-data-form", (req, res) => {
   const query = "INSERT INTO word (words) VALUES (?)";
   connection.query(query, [savedText], (err, result) => {
     if (err) {
-      console.error("Something happend while trying to save the data in DB:", err);
+      console.error(
+        "Something happend while trying to save the data in DB:",
+        err
+      );
       return res.status(500).send("Couldn't save the data");
     }
   });
 });
 
-app.get('/get-data', (req, res) => {
-  const query = 'SELECT * FROM wordsdb.word';
+app.get("/get-data", (req, res) => {
+  const query = "SELECT * FROM wordsdb.word";
   connection.query(query, (err, result) => {
     if (err) {
       console.error("Getting the data failed", err);
     }
     res.json(result);
     console.log("Got the data succesfully");
-  })
-})
+  });
+});
+
+app.get("/full-word", (req, res) => {
+  const word = req.query.word;
+  res.send(`<p>${word}</p>`);
+});
 
 app.listen(PORT, (error) => {
   if (!error)
