9772a914b1eb3e337c5111946d8add2f7ca90e1a
RobertMarc19 <mogyorosi.robert08@gmail.com>
2024-12-17 14:48:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+document
+  .getElementById("wordsDB")
+  .addEventListener("submit", async function (event) {
+    const word = document.getElementById("savedText").value;
+    try {
+      const response = await fetch("/save-data-form", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ savedText: word }),
+      });
+      if (response.ok) {
+        const result = await response.text();
+        console.log("Text stored succesfully");
+      }
+    } catch (error) {
+      console.error("eroare", error);
+    }
+  });
+
+async function displayData() {
+  try {
+    const response = await fetch("http://localhost:3000/get-data");
+    const data = await response.json();
+    data.forEach((row) => {
+      const wordsInList = `<li>${row.words}</li>`;
+      document.querySelector("ul").insertAdjacentHTML("beforeend", wordsInList);
+    });
+  } catch (error) {
+    console.log("You did not get the data!");
+  }
+}
+displayData();
---
+++ server/index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const connection = require("../database/connection");
+const path = require("path");
+const app = express();
+const cors = require('cors');
+const PORT = 3000;
+
+app.use(cors()); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "..")));
+
+app.get("/Pastebin", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "index.html"));
+});
+
+app.post("/save-data-form", (req, res) => {
+  const savedText = req.body.savedText;
+  const query = "INSERT INTO word (words) VALUES (?)";
+  connection.query(query, [savedText], (err, result) => {
+    if (err) {
+      console.error("Something happend while trying to save the data in DB:", err);
+      return res.status(500).send("Couldn't save the data");
+    }
+  });
+});
+
+app.get('/get-data', (req, res) => {
+  const query = 'SELECT * FROM wordsdb.word';
+  connection.query(query, (err, result) => {
+    if (err) {
+      console.error("Getting the data failed", err);
+    }
+    res.json(result);
+    console.log("Got the data succesfully");
+  })
+})
+
+app.listen(PORT, (error) => {
+  if (!error)
+    console.log(
+      "Server is Successfully Running, " +
+        "and App is listening on port " +
+        PORT
+    );
+  else console.log("Error occurred, server can't start", error);
+});
