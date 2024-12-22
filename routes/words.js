9e94ef35da263fa47975d6552e60b255a1640cbe
RobertMarc19 <mogyorosi.robert08@gmail.com>
2024-12-22 12:05:34 +0200

    Update words.js

--- routes/words.js
+++ routes/words.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const connection = require("../server/database/connection");
 
-
 router.post("/save-data-form", (req, res) => {
   const savedText = req.body.savedText;
   const query = "INSERT INTO word (words) VALUES (?)";
@@ -26,9 +25,16 @@ router.get("/get-data", (req, res) => {
   });
 });
 
-router.get("/full-word", (req, res) => {
-  const word = req.query.word;
-  res.send(`<p>${word}</p>`);
+router.get("/word/:id", (req, res) => {
+  const wordId = req.params.id;
+  const query = "SELECT words FROM wordsdb.word WHERE id = ?";
+  connection.query(query, [wordId], (err, result) => {
+    if (err) {
+      console.error("You couldn't get the word:", err);
+      return res.status(500).send("Couldn't fetch the word");
+    }
+    res.send(`<p>${result[0].words}</p>`);
+  });
 });
 
 module.exports = router;
